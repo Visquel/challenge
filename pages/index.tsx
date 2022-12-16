e4635fb5cdacb47bb58f71e6ca521e857f2e7168
Visquel De los Santos <visquel0208@gmail.com>
2022-12-16 15:55:09 -0400

    Deleting unuse imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import RightSide from "../components/rightSide";
-import styles from '../styles/Home.module.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./signin";
 
 export default function Root() {
   return (
-
     
     <>
       <Signin />
